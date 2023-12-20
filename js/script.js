768b78a59c959e4c387db83c47092f0cbb3a9e89
Sarah <75185314+sarisp3260@users.noreply.github.com>
2023-12-20 16:20:25 -0500

    Sección Footer, mapa añadido, falta corregir error formulario

--- js/script.js
+++ js/script.js
@@ -22,3 +22,39 @@ window.onload = function() {
         });
     });
 };
+
+// Map config start
+
+let bounds = L.latLngBounds([
+    [12.437303, -81.9289],
+    [-4.2272, -66.8698],
+  ]);
+  
+  let map = L.map("map", {
+    center: [4.710989, -74.072092],
+    zoom: 10,
+    maxBounds: bounds,
+    maxBoundsViscosity: 1.0,
+  });
+  
+  let Stadia_AlidadeSmoothDark = L.tileLayer(
+    "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}",
+    {
+      minZoom: 0,
+      maxZoom: 20,
+      ext: "png",
+      bounds: bounds,
+    }
+  ).addTo(map);
+  
+  let lugares = [
+    { nombre: "Plaza de Bolívar", coordenadas: [4.5981, -74.0758] },
+    { nombre: "Zona T", coordenadas: [4.6686, -74.0549] },
+    ,
+    { nombre: "Norte", coordenadas: [4.710989, -74.072092] },
+  ];
+  
+  lugares.forEach(function (lugar) {
+    let marker = L.marker(lugar.coordenadas).addTo(map);
+    marker.bindPopup("<b>" + lugar.nombre + "</b>");
+  });
